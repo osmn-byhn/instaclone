1ccfd5603dbb645457ef85d8e159ee18be4c1b26
osmn-byhn <osmanbeyhan12@gmail.com>
2023-03-18 19:53:06 +0300

    Basic code commit from server folder

--- server/server.js
+++ server/server.js
@@ -1,12 +1,41 @@
-
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const config = require('./config')
 const app = express()
-const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+/*
+const signRouter = require('./router/signRouter')
+const loginRouter = require('./router/loginRouter')
+const todoRouter = require('./router/todoRouter')
+const userRouter = require('./router/userRouter')
+*/
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+config()
+
+/*
+app.use('/signup', signRouter)
+app.use('/login', loginRouter)
+app.use('/todo', todoRouter)
+app.use('/user', userRouter)
+*/
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server running on: ' + process.env.PORT);
+    mongoose.set("strictQuery", false);
+    mongoose
+        .connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(console.log("CONNECT TO DB..."))
+        .catch((err) => err)
 })
